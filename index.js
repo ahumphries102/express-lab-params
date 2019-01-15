e0e8e9db7d9115dd4c110cc734f6ca370f36bf91
AJ <ahumphries102@gmail.com>
2019-01-15 16:11:01 -0600

    Magic8 Complete

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ let app = express()
 //   res.send(`hello ${req.params.name}`)
 // })
 
-let calcTip = (total, tipPercentage)=>{
-	return total*tipPercentage/100
-}
+// let calcTip = (total, tipPercentage)=>{
+// 	return total*tipPercentage/100
+// }
 
-app.get('/tip/:total/:tipPercentage', (req, res) => {
-  res.send(`Total tip ${calcTip(req.params.total, req.params.tipPercentage)}`)
+// app.get('/tip/:total/:tipPercentage', (req, res) => {
+//   res.send(`Total tip ${calcTip(req.params.total, req.params.tipPercentage)}`)
+// })
+
+let magicEight = ()=>{
+	let arr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+	let randVal = arr[Math.floor(Math.random()*(arr.length+1))]
+	
+	return randVal
+}
+app.get('/magic/Will%20I%20Be%20A%20Millionaire', (req, res) => {
+  res.send(`Shake Shake ${magicEight()}`)
 })
 
 app.listen(9000)
