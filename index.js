d36d9d075d12773b57584e3eea5f352a35893f82
AJ <ahumphries102@gmail.com>
2019-01-15 15:46:10 -0600

    Greetings Done

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 let express = require('express')
 let app = express() 
 
-app.get('/greeting/:name', (req, res) => {
-  res.send(`hello ${req.params.name}`)
-})
-
-// app.get('/:book',(req,res)=>{
-// 	let book = fileStream.readStream(`book/$(req.params.book).txt`)
-// 	book.pipe(response)
+// app.get('/greeting/:name', (req, res) => {
+//   res.send(`hello ${req.params.name}`)
 // })
+
+let calcTip = (total, tipPercentage)=>{
+	return total*tipPercentage/100
+}
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  res.send(`Total tip ${calcTip(req.params.total, req.params.tipPercentage)}`)
+})
 
 app.listen(9000)
